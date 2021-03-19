ec60e664550802116916fdfa1e0378a5bd53a2f1
Irokanade <67633095+Irokanade@users.noreply.github.com>
2021-03-19 15:24:59 +0800

    Add files via upload
    intial commit

---
+++ python_string.py
@@ -0,0 +1,16 @@
+a = "there are 3 books and 6 pencils here"
+upperCase = 0;
+digit = 0
+
+for x in a:
+    print(x)
+
+    if x.isupper():
+        upperCase += 1
+    elif x.isdigit():
+        digit += 1
+
+
+print('length of a ' + str(len(a)))
+print('number of digits ' + str(digit))
+print('nubmer of upper case ' + str(upperCase))
