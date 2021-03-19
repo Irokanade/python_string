2cbdd99c1349a75083a6ff61aca2dce05f7c25e5
Irokanade <67633095+Irokanade@users.noreply.github.com>
2021-03-19 15:30:59 +0800

    isspace added

--- python_string.py
+++ python_string.py
@@ -1,6 +1,7 @@
 a = "there are 3 books and 6 pencils here"
 upperCase = 0;
 digit = 0
+space = 0
 
 for x in a:
     print(x)
@@ -9,8 +10,11 @@ for x in a:
         upperCase += 1
     elif x.isdigit():
         digit += 1
+    elif x.isspave():
+        space += 1
 
 
 print('length of a ' + str(len(a)))
 print('number of digits ' + str(digit))
 print('nubmer of upper case ' + str(upperCase))
+print('number of space ' + str(space))
