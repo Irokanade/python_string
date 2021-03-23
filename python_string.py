bdb4e08e3414c9743fdd095d2aaaa782ecc1dc8b
Irokanade <67633095+Irokanade@users.noreply.github.com>
2021-03-23 08:38:58 +0800

    很煩的對齊 by lee*lin success

--- python_string.py
+++ python_string.py
@@ -16,13 +16,28 @@ for x in a:
 
 
 lowerCase = len(a) - upperCase - digit - space
-print('{:>19}'.format('length: ') + '{:>2}'.format(str(len(a))))
+
+x = len(str(len(a)))
+#print (('{0:>{x}}'.format('foo', x=x)))
+#print('{:>19}'.format('length: ')), print (('{0:>{x}}'.format(str(len(a)), x=x)))
+
+print('{:>19}'.format('length: ') + (('{0:>{x}}'.format(str(len(a)), x=x))))
+print('{:>19}'.format('digit numbers: ') + (('{0:>{x}}'.format(str(digit), x=x))))
+print('{:>19}'.format('lowercase letters: ') + (('{0:>{x}}'.format(str(upperCase), x=x))))
+print('{:>19}'.format('uppercase letters: ') + (('{0:>{x}}'.format(str(lowerCase), x=x))))
+print('{:>19}'.format('spaces: ') + (('{0:>{x}}'.format(str(space), x=x))))
+
+'''
+print('{:>19}'.format('length: ') + '{0:>2}'.format(str(len(a))))
 print('{:>19}'.format('digit numbers: ') + '{:>2}'.format(str(digit)))
 print('{:>19}'.format('lowercase letters: ') + '{:>2}'.format(str(upperCase)))
 print('{:>19}'.format('uppercase letters: ') + '{:>2}'.format(str(lowerCase)))
 print('{:>19}'.format('spaces: ') + '{:>2}'.format(str(space)))
 
-#print('number of digits ' + str(digit))
-#print('nubmer of upper case ' + str(upperCase))
-#print('number of space ' + str(space))
-#print('number of lower case ' + str(lowerCase))
+print('number of digits ' + str(digit))
+print('nubmer of upper case ' + str(upperCase))
+print('number of space ' + str(space))
+print('number of lower case ' + str(lowerCase))
+
+
+'''
